3a55624f38e992be0820386955c9b8dec5d6392f
tabsus <tabsus@mail.ru>
2021-02-13 16:56:15 +0700

    added product-slider

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,20 @@ $(function () {
 
    var mixer = mixitup('.products__inner-container');
 
+   $(".rate-star").rateYo({
+      rating: 5,
+      starWidth: "12px",
+      readOnly: true
+   });
+
+   $('.product-slider__inner').slick({
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      dots: true,
+      arrows: false,
+      speed: 1500,
+      autoplay: true,
+      autoplaySpeed: 6000
+   });
+
 });
